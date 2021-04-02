09db2bfc5e40add6883db4fae067678ea6351489
Matt Landers <matt@platypi.io>
2021-04-02 15:19:43 -0500

    added link in nav

--- lib/components/Nav/Nav.tsx
+++ lib/components/Nav/Nav.tsx
@@ -8,8 +8,13 @@ const Nav = () => {
     <nav className={styles.nav}>
       <div className={styles.container}>
         <Link href="/">
-          <a href="/">
+          <a className={styles.logo} href="/">
             <span>FollowA.Dev</span>
+          </a>
+        </Link>
+        <Link href="/devsubmission">
+          <a className={styles.linkbutton} href="/devsubmission">
+            Submit a Dev
           </a>
         </Link>
         <div className={styles.toggle}>
