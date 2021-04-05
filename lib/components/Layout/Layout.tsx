7fd2d35a753528af8b7ae6e707d4ca70e4ebbf08
Matt Landers <matt@platypi.io>
2021-04-05 09:22:10 -0500

    added language to head so google doesn't try and translate

--- lib/components/Layout/Layout.tsx
+++ lib/components/Layout/Layout.tsx
@@ -9,6 +9,8 @@ const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Head>
+        <meta name="google" content="notranslate" />
+        <meta http-equiv="Content-Language" content="en" />
         <title>FollowA.Dev</title>
       </Head>
       <div className={styles.layout}>
